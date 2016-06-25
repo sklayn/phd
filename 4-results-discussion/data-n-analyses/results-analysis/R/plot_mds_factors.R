d8b891c9adf644d9ef054f25cb5dbbf6b61e7a85
Stefania Klayn <stefaniaklayn@yahoo.com>
2016-06-25 22:10:24 +0300

    Final final version of exploratory MDS plot for sand.

--- 4-results-discussion/data-n-analyses/results-analysis/R/plot_mds_factors.R
+++ 4-results-discussion/data-n-analyses/results-analysis/R/plot_mds_factors.R
@@ -38,22 +38,45 @@ plot_mds <- function(mds.obj, stations) {
   
   # import libraries
   library(ggplot2)
+  library(plyr)
   library(vegan)
   
   # extract the MDS scores and create data frame for plotting
   scrs <- as.data.frame(scores(mds.obj, display = "sites"))
   
   # add labels to use for grouping
-  scrs <- cbind(scrs, stations) 		
+  scrs <- cbind(scrs, stations)
+  names(scrs) <- c("NMDS1", "NMDS2", "stations")
+  
+  # create a separate data frame with mean NMDS coordinates for 
+  # the group annotations 
+  mds.group.labs <- ddply(scrs, 
+                          .(stations), 
+                          summarize, 
+                          NMDS1.m = mean(NMDS1), 
+                          NMDS2.m = mean(NMDS2)) 
+  
+  # make a custom colour scale (to match text label colours with point colours) 
+  custom.cols <- brewer.pal(n = length(levels(scrs$stations)), name = "Set2")
+  names(custom.cols) <- levels(scrs$stations) 
+  custom.col.scale <- scale_color_manual(name = "", values = custom.cols)
   
   p <- ggplot(scrs, aes(x = NMDS1, y = NMDS2, colour = stations)) + 
-    geom_text(aes(label = stations), size = 3, position = "identity", show.legend = FALSE) +
+    geom_point(size = 2, position = "identity", show.legend = FALSE) +
     stat_ellipse(show.legend = FALSE) +
-    scale_color_brewer(palette = "Set2") +
-    theme_bw()
+    custom.col.scale + 
+    theme_bw() + 
+    # add group annotations inside the ellipses 
+    annotate("text", 
+             x = mds.group.labs$NMDS1.m, 
+             y = mds.group.labs$NMDS2.m, 
+             label = mds.group.labs$stations, 
+             colour = custom.cols, 
+             size = 5)
   
   return(p)
 }
+
 
 
 ### FIX THIS ONE! CHECK NAMES OF COLUMNS OF DF - EXTRACT_ENVFIT
