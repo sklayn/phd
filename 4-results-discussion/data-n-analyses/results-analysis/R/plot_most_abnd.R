b72472a76155122ee5a6ff1a02a8e545ed478a02
Stefania Klayn <stefaniaklayn@yahoo.com>
2016-07-19 17:51:18 +0300

    Plotting function - sp by their total abundnace.

---
+++ 4-results-discussion/data-n-analyses/results-analysis/R/plot_most_abnd.R
@@ -0,0 +1,43 @@
+plot_most_abnd_sp <- function(abnd.df, gr.factor, nb.sp = 15) {
+  ## plots transformed abundance of the specified number of most abundant species
+  ## and colors the points by the levels of the supplied factor.
+  
+  library(reshape2)
+  library(ggplot2)
+  
+  # calculate the total abundance of each species, then sort in descending order
+  tot.abnd <- colSums(abnd.df)
+  tot.abnd.sorted <- sort(tot.abnd, decreasing = TRUE, index.return = TRUE) # make sure to return column indices, not names!
+  
+  # extract the given number of species (columns) from the abundance data frame - by their index
+  abnd.sub <- abnd.df[, tot.abnd.sorted$ix[1:nb.sp]]
+  
+  # add the factor column to the data frame, if it's not already there
+  abnd.sub <- cbind(abnd.sub, group = gr.factor)
+  
+  # convert the df to long format -> easier for ggplot2 to handle  
+  abnd.melted <- melt(abnd.sub, id.vars = "group") 
+  
+  # this doesn't keep row names, which here denote stations/replicates (does keep
+  # row order, so easy to recover, if needed)
+  
+  ## use custom transformation for abundance values (log(y/min + 1)) - as in the 
+  ## mvabund package
+  # calculate the minimum non-0 value in the dataset 
+  min.val <- min(abnd.melted$value[abnd.melted$value > 0]) 
+  abnd.melted$value.tr <- log(abnd.melted$value / min.val + 1)
+  
+  ggplot(abnd.melted, aes_string(x = "variable", 
+                                 y = "value.tr", 
+                                 colour = "group", 
+                                 shape = "group")) + 
+    geom_point(size = 2.5) + 
+    # reverse the order of x axis, so highest-contributing species are on top
+    scale_x_discrete(name = "", limits = rev(levels(abnd.melted$variable))) + 
+    scale_y_continuous(name = "Abundance (log(y/min + 1))") + 
+    coord_flip() + # put species on y axis - easier to read
+    theme_bw()
+}
+
+
+### maybe the same, but by station? just to see
