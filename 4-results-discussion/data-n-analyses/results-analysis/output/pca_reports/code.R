2fa89130a51580889ca5ea010bf76b9387e68494
Stefania Klayn <stefaniaklayn@yahoo.com>
2018-07-19 17:40:52 +0300

    Redid some of the environmental PCAs; added LUSI to water column PCAs & fixed final PCA visualizations for sand and seagrass (2013-14).

--- 4-results-discussion/data-n-analyses/results-analysis/output/pca_reports/code.R
+++ 4-results-discussion/data-n-analyses/results-analysis/output/pca_reports/code.R
@@ -1,31 +1,54 @@
 barplot(res$eig[,2], names.arg = 1:nrow(res$eig))
 drawn <-
-c("9", "7", "87", "41", "84", "26", "6", "27", "10", "83", "44", 
-"86", "72", "18", "40", "8", "16", "19")
+c("2", "26", "7", "1", "3", "10", "5", "18", "14", "21", "19", 
+"11")
 plot.PCA(res, select = drawn, axes = 1:2, choix = 'ind', invisible = 'quali', title = '', cex = cex)
 wilks.p <-
-structure(4.34559841713137e-05, .Names = "station")
+structure(8.22559958029619e-06, .Names = "station")
 wilks.p
 sample = sample(rownames(res$call$X), length(rownames(res$call$X)))
 res$call$X = res$call$X[sample,]
 res$ind$coord = res$ind$coord[sample[!sample %in% rownames(res$ind.sup$coord)],]
 res$ind.sup$coord = res$ind.sup$coord[sample[sample %in% rownames(res$ind.sup$coord)],]
 drawn <-
-c("9", "7", "87", "41", "84", "26", "6", "27", "10", "83", "44", 
-"86", "72", "18", "40", "8", "16", "19")
+c("2", "26", "7", "1", "3", "10", "5", "18", "14", "21", "19", 
+"11")
 hab <-
 "station"
 plotellipses(res, axes = 1:2, invisible = 'quali', select = drawn, keepvar = hab, title = '', cex = cex)
 drawn <-
-c("shoot_count", "bg_biomass_wet", "ag_biomass_wet")
+c("Ntotal", "LUSI", "chl_a")
 plot.PCA(res, select = drawn, axes = 1:2, choix = 'var', title = '', cex = cex)
 drawn <-
-c("Gradina", "Otmanli", "Poda")
+c("Poda", "Gradina", "Otmanli")
 plot.PCA(res, select = drawn, axes = 1:2, choix = 'ind', invisible = c('ind', 'ind.sup'), title = '', cex = cex)
+drawn <-
+c("25", "18", "3", "26", "4", "6", "14", "11", "9", "28", "2", 
+"16", "24", "8", "12", "19", "10")
+plot.PCA(res, select = drawn, axes = 3:4, choix = 'ind', invisible = 'quali', title = '', cex = cex)
+wilks.p <-
+structure(0.0688114100435798, .Names = "station")
+wilks.p
+sample = sample(rownames(res$call$X), length(rownames(res$call$X)))
+res$call$X = res$call$X[sample,]
+res$ind$coord = res$ind$coord[sample[!sample %in% rownames(res$ind.sup$coord)],]
+res$ind.sup$coord = res$ind.sup$coord[sample[sample %in% rownames(res$ind.sup$coord)],]
+drawn <-
+c("25", "18", "3", "26", "4", "6", "14", "11", "9", "28", "2", 
+"16", "24", "8", "12", "19", "10")
+hab <-
+"station"
+plotellipses(res, axes = 3:4, invisible = 'quali', select = drawn, keepvar = hab, title = '', cex = cex)
+drawn <-
+c("NH4", "chl_a", "secchi")
+plot.PCA(res, select = drawn, axes = 3:4, choix = 'var', title = '', cex = cex)
+drawn <-
+c("Ropotamo", "Otmanli")
+plot.PCA(res, select = drawn, axes = 3:4, choix = 'ind', invisible = c('ind', 'ind.sup'), title = '', cex = cex)
 res.hcpc = HCPC(res, nb.clust = -1, graph = FALSE)
 drawn <-
-c("9", "7", "87", "41", "84", "26", "6", "27", "10", "83", "44", 
-"86", "72", "18", "40", "8", "16", "19")
+c("2", "26", "7", "1", "3", "10", "5", "18", "14", "21", "19", 
+"11")
 plot.HCPC(res.hcpc, choice = 'map', draw.tree = FALSE, select = drawn, title = '')
-dimdesc(res, axes = 1:1)
+dimdesc(res, axes = 1:3)
 res.hcpc$desc.var
